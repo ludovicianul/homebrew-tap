b1f93cf64ae97b6ac279ee67e53b5eb934688403
Madalin Ilie <iliemad@gmail.com>
2022-09-08 19:58:49 +0300

    Add words version 1.4

---
+++ words.rb
@@ -0,0 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Words < Formula
+  desc "A set of word-based puzzle games for the CLI while you wait for the build to run"
+  homepage ""
+  url "https://github.com/ludovicianul/words/releases/download/words-1.4/words_macos_amd64_1.4.tar.gz"
+  sha256 "50a93106411fe6e0084cc1fdba3fd6c422e8556a6f83a5ccca2a184a0ccf8a9b"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    #system "./configure", *std_configure_args, "--disable-silent-rules"
+    bin.install 'words'
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test words`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
