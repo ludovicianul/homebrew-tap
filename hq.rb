5b5949924f415a6d1a1fdcb7664dcb9cab773c8b
Madalin Ilie <iliemad@gmail.com>
2023-09-28 10:16:24 +0300

    Update to latest version of hq

--- hq.rb
+++ hq.rb
@@ -3,9 +3,28 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Hq < Formula
   desc "lightweight command line HTML processor using CSS and XPath selectors"
-  homepage ""
-  url "https://github.com/ludovicianul/hq/releases/download/hq-1.3.0/hq_macos_amd64_1.3.0.tar.gz"
-  sha256 "65dbea72b36409df2975f954e4e1c866264793cdd1ad1ec794a899e09743fae0"
+  homepage "https://github.com/ludovicianul/hq"
+
+  if OS.mac?
+        if Hardware::CPU.intel?
+          url "https://github.com/ludovicianul/hq/releases/download/hq-1.3.2/hq_macos_amd64_1.3.2.tar.gz"
+          sha256 ""
+        elsif Hardware::CPU.arm?
+          url "https://github.com/ludovicianul/hq/releases/download/hq-1.3.2/hq_macos_arm64_1.3.2.tar.gz"
+          sha256 "fa7d82f66ce187bd8bd1c443c70aa9584dea1d933b9984b2b7080a29fbe0397a"
+        end
+    elsif OS.linux?
+        if Hardware::CPU.intel?
+          url "https://github.com/ludovicianul/hq/releases/download/hq-1.3.2/hq_linux_amd64_1.3.2.tar.gz"
+          sha256 "d642e250c66185673745416adc94ee6f9d287ddd7e9c020cab6492d4a3ec6c1d"
+        elsif Hardware::CPU.arm?
+          url "https://github.com/ludovicianul/hq/releases/download/hq-1.3.2/hq_linux_arm64_1.3.2.tar.gz"
+          sha256 "f3a5d0d8853cd2febe441a2d731df3849f4376758a64d105a20b84835ef02dbe"
+        end
+    else
+      odie "Please use the uberjar version: https://github.com/ludovicianul/hq/releases/download/hq-1.3.2/hq_uberjar_1.3.2.tar.gz"
+    end
+
   license "MIT"
 
   # depends_on "cmake" => :build
