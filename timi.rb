51de82e6f6c45a174a9e58ce1d7736c2f1903dc9
Madalin Ilie <iliemad@gmail.com>
2025-04-07 09:58:50 +0300

    Update to latest version of timi

--- timi.rb
+++ timi.rb
@@ -2,27 +2,27 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Timi < Formula
-  desc "Timi is a blazing-fast, local-first command-line tool to track how you spend your time."
+  desc "Timi is a command line application that allows you to chat with your git history."
   homepage "https://github.com/ludovicianul/timi"
 
   if OS.mac?
       if Hardware::CPU.intel?
-        url "https://github.com/ludovicianul/timi/releases/download/timi-1.0.0/timi_macos_amd64_1.0.0.tar.gz"
-        sha256 "efa8f1646a81822d18a6ba6b5d3014e61abb95d6710386117e459350f34802af"
+        url "https://github.com/ludovicianul/timi/releases/download/timi-1.0.2/timi_macos_amd64_1.0.2.tar.gz"
+        sha256 "ab4c93f9b204989df0c0e362f8bbb484be369658ac137babaa4c6661a1a83356"
       elsif Hardware::CPU.arm?
-        url "https://github.com/ludovicianul/timi/releases/download/timi-1.0.0/timi_macos_arm64_1.0.0.tar.gz"
-        sha256 "5395d9c668eca15b323f62b27fb3e8243ee5e0d44892a4510d54701a21c76d30"
+        url "https://github.com/ludovicianul/timi/releases/download/timi-1.0.2/timi_macos_arm64_1.0.2.tar.gz"
+        sha256 "abca514027104cb18e095da845f245592668f5275c463a5fca8652160928c928"
       end
   elsif OS.linux?
       if Hardware::CPU.intel?
-        url "https://github.com/ludovicianul/timi/releases/download/timi-1.0.0/timi_linux_amd64_1.0.0.tar.gz"
-        sha256 "11fc97a0430f3228776e1e0d3ed7c4cf065d4a0220b13866596e5600be5c76bf"
+        url "https://github.com/ludovicianul/timi/releases/download/timi-1.0.2/timi_linux_amd64_1.0.2.tar.gz"
+        sha256 "3f1781c61658992b33acfd14559c684866756a6ed79ca61a96a2d157910336ee"
       elsif Hardware::CPU.arm?
-        url "https://github.com/ludovicianul/timi/releases/download/timi-1.0.0/timi_linux_arm64_1.0.0.tar.gz"
-        sha256 "4ad1850a45a88598405c0afd3744a7e7d1c918fad1d6e415013e4febc1728ce4"
+        url "https://github.com/ludovicianul/timi/releases/download/timi-1.0.2/timi_linux_arm64_1.0.2.tar.gz"
+        sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
       end
   else
-    odie "Please use the uberjar version: https://github.com/ludovicianul/timi/releases/download/timi-1.0.0/timi_uberjar_1.0.0.tar.gz"
+    odie "Please use the uberjar version: https://github.com/ludovicianul/timi/releases/download/timi-1.0.2/timi_uberjar_1.0.2.tar.gz"
   end
 
   license "MIT"
