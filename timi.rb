dce1454de681149d372b2c9cddfd0cbee3eba088
Madalin Ilie <iliemad@gmail.com>
2026-05-08 08:39:51 +0300

    Update to latest version of timi

--- timi.rb
+++ timi.rb
@@ -7,22 +7,22 @@ class Timi < Formula
 
   if OS.mac?
       if Hardware::CPU.intel?
-        url "https://github.com/ludovicianul/timi/releases/download/timi-1.0.8/timi_macos-15_amd64_1.0.8.tar.gz"
+        url "https://github.com/ludovicianul/timi/releases/download/timi-1.0.9/timi_macos-15_amd64_1.0.9.tar.gz"
         sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
       elsif Hardware::CPU.arm?
-        url "https://github.com/ludovicianul/timi/releases/download/timi-1.0.8/timi_macos_arm64_1.0.8.tar.gz"
-        sha256 "bf1ba9fdaac3fff085ce11297d405dafbb825de6834d09d35b78e53faa8adf10"
+        url "https://github.com/ludovicianul/timi/releases/download/timi-1.0.9/timi_macos_arm64_1.0.9.tar.gz"
+        sha256 "024bf75db0f1ff555a61156e631f37328e09958c8ad51652ce96117a03470860"
       end
   elsif OS.linux?
       if Hardware::CPU.intel?
-        url "https://github.com/ludovicianul/timi/releases/download/timi-1.0.8/timi_linux_amd64_1.0.8.tar.gz"
-        sha256 "bfb8853ae1f50371ed6e4e9988a0228a13bcc1f694006bab883f68d68cfe15e6"
+        url "https://github.com/ludovicianul/timi/releases/download/timi-1.0.9/timi_linux_amd64_1.0.9.tar.gz"
+        sha256 "1253ac5f6c79b6151c4f8ff41dc621fa53dfce2785246da238305396b2517e1e"
       elsif Hardware::CPU.arm?
-        url "https://github.com/ludovicianul/timi/releases/download/timi-1.0.8/timi_linux_arm64_1.0.8.tar.gz"
-        sha256 "1de4c1b873cab5ac6d19e1fdb7ce4287da25b961276884d17190a8ef49047ff9"
+        url "https://github.com/ludovicianul/timi/releases/download/timi-1.0.9/timi_linux_arm64_1.0.9.tar.gz"
+        sha256 "2cb355770e51400bb51f0570586806be131f5d3ae8f45bd68032393f93bcb6ac"
       end
   else
-    odie "Please use the uberjar version: https://github.com/ludovicianul/timi/releases/download/timi-1.0.8/timi_uberjar_1.0.8.tar.gz"
+    odie "Please use the uberjar version: https://github.com/ludovicianul/timi/releases/download/timi-1.0.9/timi_uberjar_1.0.9.tar.gz"
   end
 
   license "MIT"
